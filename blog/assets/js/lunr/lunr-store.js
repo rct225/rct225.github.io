77b7edb63e34b824017acf724875a73f3b725f5f
rct225 <rct225@users.noreply.github.com>
2023-02-15 01:08:45 +0000

    deploy: rct225/random-bits-of-rob@709f7cff1a4224763d3eddcc4e0f84021ddd597d

--- blog/assets/js/lunr/lunr-store.js
+++ blog/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "First Post",
+        "excerpt":"This is another attempt at blogging for me. I tried this once before when I was ill in 2015-2016, but gave up once I was healthy. Not sure what the motivation is this time … boredom, the need to create something, a writing exercise, heading towards 50 … I suppose...","categories": ["General Thoughts"],
+        "tags": ["General Thoughts","nothing in particular"],
+        "url": "/blog/general%20thoughts/first-post/",
+        "teaser": null
+      }]
